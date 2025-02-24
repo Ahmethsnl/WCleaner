2d2de67f9b5dddb86273debb4027588287e7a883
Ahmet Şanlı <87249395+Ahmethsnl@users.noreply.github.com>
2025-02-24 19:31:43 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -28,19 +28,21 @@ add_alias "wclean" "$ZSH_RC"
 
 CURRENT_DIR="$(pwd)"
 echo -e "\nŞu an bu dizindesiniz: \033[1;33m$CURRENT_DIR\033[0m"
+GIT_FOLDER="$(basename $(git rev-parse --show-toplevel 2>/dev/null))"
 
-if [[ "$CURRENT_DIR" == *WesaClean* ]]; then
-  read -p "Şu an WesaClean klasöründesiniz. Bu klasörü silmek istiyor musunuz? (y/n) " confirm
+if [[ -n "$GIT_FOLDER" ]]; then
+  read -p "Şu an '$GIT_FOLDER' klasöründesiniz. Bu klasörü silmek istiyor musunuz? (y/n) " confirm
   if [[ "$confirm" == [yY] ]]; then
     cd ~ 
     rm -rf "$CURRENT_DIR"
-    echo "WesaClean klasörü silindi."
+    echo "'$GIT_FOLDER' klasörü silindi."
   else
     echo "Silme işlemi iptal edildi."
   fi
 else
-  echo "WesaClean klasöründe değilsiniz, silme işlemi yapılmadı."
+  echo "Bu bir Git deposu değil veya Git klasör adını belirleyemedim. Manuel kontrol etmen gerekebilir."
 fi
+
 
 echo -e "\nTerminalinizi kapatıp açın veya '\033[1;34msource ~/.bashrc\033[0m' komutunu çalıştırın."
 echo -e "Bundan sonra '\033[1;34mwclean\033[0m' komutuyla WesaClean'i çalıştırabilirsiniz! 🚀"
